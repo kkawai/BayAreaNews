cd27de24c36145841c179c84660edc0a9e4dc59b
kkawai <kkawai2@gmail.com>
2023-12-04 22:26:49 -0800

    don't allow getting featured articles take more than 3 seconds to finish since it blocks regular articles from fetching.  in future, launch both jobs in parallel

--- app/src/main/java/com/kk/android/bayareanews/presentation/MainActivity.kt
+++ app/src/main/java/com/kk/android/bayareanews/presentation/MainActivity.kt
@@ -7,8 +7,12 @@ import com.google.firebase.ktx.Firebase
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import com.google.firebase.remoteconfig.ktx.remoteConfig
 import com.kk.android.bayareanews.NewsReaderApp
+import com.kk.android.bayareanews.common.MLog
 import com.kk.android.bayareanews.ui.theme.BayAreaNewsTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -24,5 +28,15 @@ class MainActivity : ComponentActivity() {
             .addOnCompleteListener(this) { task ->
                 NewsReaderApp.app.remoteConfigResponse.complete(task.isSuccessful)
             }
+        GlobalScope.launch {
+            try {
+                if (!NewsReaderApp.app.remoteConfigResponse.isCompleted) {
+                    MLog.i("nnnnn","RssRepositoryImpl about to delay 3 seconds")
+                    delay(3000)
+                    NewsReaderApp.app.remoteConfigResponse.complete(false)
+                    MLog.i("nnnnn","RssRepositoryImpl finished delay of 3 seconds")
+                }
+            }catch (ignored: Throwable){}
+        }.start()
     }
 }
