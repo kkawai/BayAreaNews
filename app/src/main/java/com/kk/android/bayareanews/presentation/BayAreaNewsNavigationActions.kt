c01b0c631e43c1c265897d431471f4ff25c87761
kkawai <kkawai2@gmail.com>
2023-12-08 20:13:24 -0800

    fixed webview navigation issue; removed popUpTo since not part of the nav drawer

--- app/src/main/java/com/kk/android/bayareanews/presentation/BayAreaNewsNavigationActions.kt
+++ app/src/main/java/com/kk/android/bayareanews/presentation/BayAreaNewsNavigationActions.kt
@@ -47,14 +47,14 @@ class BayAreaNewsNavigationActions(navController: NavHostController) {
             // Pop up to the start destination of the graph to
             // avoid building up a large stack of destinations
             // on the back stack as users select items
-            popUpTo(navController.graph.findStartDestination().id) {
-                saveState = true
-            }
+            //popUpTo(navController.graph.findStartDestination().id) {
+            //    saveState = true
+            //}
             // Avoid multiple copies of the same destination when
             // reselecting the same item
-            launchSingleTop = true
+            //launchSingleTop = true
             // Restore state when reselecting a previously selected item
-            restoreState = true
+            //restoreState = true
         }
     }
 }
