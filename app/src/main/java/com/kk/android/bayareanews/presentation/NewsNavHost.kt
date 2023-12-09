51edfc18b3ae81acb6e49b192c0f8967fc488d24
kkawai <kkawai2@gmail.com>
2023-12-08 20:05:37 -0800

    minor fixed imports

--- app/src/main/java/com/kk/android/bayareanews/presentation/NewsNavHost.kt
+++ app/src/main/java/com/kk/android/bayareanews/presentation/NewsNavHost.kt
@@ -7,7 +7,6 @@ import androidx.navigation.NavHostController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.kk.android.bayareanews.presentation.ui.common.Screen
 import com.kk.android.bayareanews.presentation.ui.common.WebViewScreen
--- app/src/main/java/com/kk/android/bayareanews/presentation/ui/common/WebViewScreen.kt
+++ app/src/main/java/com/kk/android/bayareanews/presentation/ui/common/WebViewScreen.kt
@@ -6,25 +6,16 @@ import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.outlined.Menu
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.viewinterop.AndroidView
-import com.kk.android.bayareanews.R
 import com.kk.android.bayareanews.common.EncodingUtil
 
 @OptIn(ExperimentalMaterial3Api::class)
