9088cc9c8fd2ffc519a089ea9b2740bc20b2af25
kkawai <kkawai2@gmail.com>
2024-07-13 02:46:12 -0700

    version bumped for next release (not released yet)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.kk.android.bayareanews"
         minSdk = 24
         targetSdk = 34
-        versionCode = 8
-        versionName = "2.1"
+        versionCode = 9
+        versionName = "2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
